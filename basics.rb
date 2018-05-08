d73f28b2e0d7169cc85448fa28024419c7943b8b
Juan Paredes <juanchuu.jfpc@gmail.com>
2018-05-08 21:09:30 +0200

    problem in process

--- basics.rb
+++ basics.rb
@@ -1,3 +1,28 @@
-def age_in_days
+  require 'date'
+#def age_in_days(day,month,year)
+#day = 15
+#month = 04
+#year = 1997
+#return day, month, year
+#end
+#puts age_in_days(15,04,1997)
 
+#def age_in_days(day,month,year)
+   #day =  DateTime.now
+   #year= Date.new(1997,4,15)
+   #month = day + year
+   #time_until = day + year
+  # time_until.to_i
+ #  return day, month, year
+#end
+#puts age_in_days(day,month, year)
+
+
+
+
+def age_in_days(day, month, year)
+  (Date.today - Date.new(year, month, day)).to_i
+  return day, month, year
 end
+
+puts age_in_days()
